465bdd6be3c14d5cc8452a5a7265040e523413e8
Suhail-8800 <suhail2022@vitbhopal.ac.in>
2026-03-08 17:23:53 +0530

    Fix GitHub Pages routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 import Home from "./pages/Home";
 import Impact from "./pages/Impact";
 import Women from "./pages/Women";
 import Inspiration from "./pages/Inspiration";
 import Wall from "./pages/Wall";
-import Footer from "./components/Footer";
 
-export default function App(){
+function App() {
+  return (
+    <BrowserRouter basename="/womens-day">
+      <Navbar />
 
-return(
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/impact" element={<Impact />} />
+        <Route path="/women" element={<Women />} />
+        <Route path="/inspiration" element={<Inspiration />} />
+        <Route path="/wall" element={<Wall />} />
+      </Routes>
 
-<Router>
-
-<Navbar/>
-
-<Routes>
-
-<Route path="/" element={<Home/>}/>
-<Route path="/impact" element={<Impact/>}/>
-<Route path="/women" element={<Women/>}/>
-<Route path="/inspiration" element={<Inspiration/>}/>
-<Route path="/wall" element={<Wall/>}/>
-
-</Routes>
-<Footer/>
-
-</Router>
-
-);
-
+      <Footer />
+    </BrowserRouter>
+  );
 }
+
+export default App;
